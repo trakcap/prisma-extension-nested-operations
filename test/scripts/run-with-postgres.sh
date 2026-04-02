9b296dabe7af38100ff3a65c1b72a6d0a1ad5e9e
Oleg Tapinov <tapinov36@gmail.com>
2026-04-02 21:49:43 +0000

    fix test ci

--- test/scripts/run-with-postgres.sh
+++ test/scripts/run-with-postgres.sh
@@ -4,7 +4,10 @@ export DATABASE_URL=postgres://postgres:postgrespass@localhost:5433/test
 
 trap "docker compose down" EXIT
 
-docker compose up -d && sleep 1
+if [ "$1" != "ci" ]; then
+  trap "docker compose down" EXIT
+  docker compose up -d && sleep 1
+fi
 npx prisma db push
 
 $@
